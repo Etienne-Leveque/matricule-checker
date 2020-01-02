db61086f4824c2127fd3e4eee4fd219343e11a2c
Etienne Lévêque <eti.lvque@gmail.com>
2020-01-02 21:50:58 +0100

    Run with process pool

--- matricule_checker.py
+++ matricule_checker.py
@@ -1,7 +1,9 @@
+import time
 import re
 import os
 import sys
 from glob import glob
+import concurrent.futures
 
 from pdfminer.high_level import extract_text
 
@@ -9,6 +11,8 @@ if getattr(sys, "frozen", False):
     application_path = os.path.dirname(sys.executable)
 elif __file__:
     application_path = os.path.dirname(__file__)
+
+start = time.time()
 
 REPORT_PATH = os.path.join(application_path, "report.csv")
 
@@ -18,17 +22,18 @@ all_pdf_file_paths = glob(os.path.join(application_path, "*.pdf"))
 
 if not all_pdf_file_paths:
     print("No pdf files to check.")
+    sys.exit()
 
 print(f"Will check {len(all_pdf_file_paths)} pdf files: \n")
 
-for pdf_file_path in all_pdf_file_paths:
+
+def check_matricule(pdf_file_path):
     pdf_filename = os.path.basename(pdf_file_path)
     print(f"Checking {pdf_filename}...")
     matricule_match = re.match(r"(?P<filename_matricule>\d{7})", pdf_filename)
 
     if not matricule_match:
-        report.append(f"{pdf_filename};;;non")
-        continue
+        return f"{pdf_filename};;;non"
 
     filename_matricule = matricule_match["filename_matricule"]
 
@@ -50,12 +55,22 @@ for pdf_file_path in all_pdf_file_paths:
             condition = pdf_matricule == filename_matricule
 
         if condition:
-            report.append(f"{pdf_filename};{filename_matricule};{pdf_matricule};oui")
+            return f"{pdf_filename};{filename_matricule};{pdf_matricule};oui"
         else:
-            report.append(f"{pdf_filename};{filename_matricule};{pdf_matricule};non")
+            return f"{pdf_filename};{filename_matricule};{pdf_matricule};non"
     else:
-        report.append(f"{pdf_filename};{filename_matricule};;non")
+        return f"{pdf_filename};{filename_matricule};;non"
 
+
+executor = concurrent.futures.ProcessPoolExecutor()
+results = executor.map(check_matricule, all_pdf_file_paths)
+report += results
+end = time.time()
+
+print()
+print(f"Executed in {round(end - start, 3)} seconds.\n")
+
+print("Writing report...")
 
 with open(REPORT_PATH, "w+") as file_:
     file_.write("\n".join(report))
